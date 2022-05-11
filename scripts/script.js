6d438f60a72bfee6748fddc9a12ec41d537ddac5
Bryan Marler <bjamesmarler@gmail.com>
2022-05-11 10:27:37 -0400

    created array for random greeting

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
+const greet = ["Hi, ", "Hello ", "Oh, hi "];
+let rand = Math.floor(Math.random()*greet.length);
+
+let randGreet = greet[rand];
+
 function greeting() {
     let input = document.getElementById("uname").value;
     function hello() {
-        return("It's nice to meet you, " + input + "!")
+        return(randGreet + input + "!")
     }
     document.getElementById("hello").innerHTML = hello();
 }
